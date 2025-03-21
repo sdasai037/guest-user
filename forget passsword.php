2b00d7516f1840d2e224e07e401a15135193d019
desai shubham <sdesai037@rku.ac.in>
2025-03-21 12:42:53 +0530

    12-3-2005

--- forget passsword.php
+++ forget passsword.php
@@ -23,43 +23,23 @@
 
       </div>
   </div>
+
+      <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $email = htmlspecialchars($_POST['email']);
+
+
+            if (!empty($email)) {
+
+                echo "<script>alert('A reset password link has been sent to $email');</script>";
+                echo "<script>window.location.href = 'login.php';</script>";
+            } else {
+                echo "<script>alert('Please provide a valid email address.');</script>";
+            }
+        }
+        ?>
+
   <?php
     include 'footer.php';
     ?>
 
-
-  <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script> -->
-
-
-  <!-- <script>
-      $(document).ready(function() {
-          $("#login-form").validate({
-              rules: {
-                  email: {
-                      required: true,
-                      email: true
-                  }
-
-              },
-              messages: {
-                  email: {
-                      required: "Email is required.",
-                      email: "Please enter a valid email address."
-                  }
-              },
-              errorElement: "div",
-              errorPlacement: function(error, element) {
-                  error.addClass("text-danger mt-1 small");
-                  error.insertAfter(element);
-              },
-              highlight: function(element) {
-                  $(element).addClass("is-invalid");
-              },
-              unhighlight: function(element) {
-                  $(element).removeClass("is-invalid");
-              }
-          });
-      });
-  </script> -->
